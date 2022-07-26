ae9e5692f860df6bd817aa72005ec552f0dbba43
luluulemon <chen_luwei@yahoo.com.sg>
2022-07-26 14:38:14 +0800

    Lu test commit

--- src/main/java/vttp2022/workshop16/controller/controller.java
+++ src/main/java/vttp2022/workshop16/controller/controller.java
@@ -2,9 +2,22 @@ package vttp2022.workshop16.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
     private static final Logger logger = LoggerFactory.getLogger(controller.class);
+
+    @PostMapping(path="/api/boardgame", consumes="application/json")
+    public ResponseEntity<String> postUser(@RequestBody String payload){
+        
+        JsonObject body;
+
+
+    }
+
 }
