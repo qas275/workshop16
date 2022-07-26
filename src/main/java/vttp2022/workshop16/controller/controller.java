e4d0526f5b16dcfd4449ed124513e5a0323a232a
ElKayJae <lowkejun91@gmail.com>
2022-07-26 17:03:00 +0800

    update

--- src/main/java/vttp2022/workshop16/controller/controller.java
+++ src/main/java/vttp2022/workshop16/controller/controller.java
@@ -2,28 +2,33 @@ package vttp2022.workshop16.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.security.Provider.Service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.json.Json;
-import jakarta.json.JsonObject;
-import jakarta.json.JsonReader;
 import vttp2022.workshop16.model.BoardMain;
+import vttp2022.workshop16.service.GameService;
 
 @RestController
 public class controller {
+
+    @Autowired
+    GameService service;
+
     private static final Logger logger = LoggerFactory.getLogger(controller.class);
 
     @PostMapping(path="/api/boardgame", consumes="application/json")
-    public BoardMain postBoard(@RequestBody BoardMain boardMain){
-        
-        return boardMain;
+    public ResponseEntity<String> postBoard(@RequestBody BoardMain boardMain){
+        service.saveRedis(boardMain);
+        // final JsonObject resp = Json.createObjectBuilder().build();
+        return ResponseEntity.ok(boardMain.toString());
     }
     // public ResponseEntity<String> postUser(@RequestBody String payload){
         
@@ -38,6 +43,6 @@ public class controller {
 
 
 
-    }
+    
 
 }
