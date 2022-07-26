3bf0c6aaf1f0a28dac1398276173c9dc7794d66b
qas275 <weishunlim93@hotmail.com>
2022-07-26 22:45:30 +0800

    modified service

--- src/main/java/vttp2022/workshop16/service/service.java
+++ src/main/java/vttp2022/workshop16/service/service.java
@@ -1,11 +1,32 @@
 package vttp2022.workshop16.service;
 
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+
+import jakarta.json.JsonObject;
+import vttp2022.workshop16.model.BoardMain;
+
+
 @Service
-public class service {
-    private static final Logger logger = LoggerFactory.getLogger(service.class);
+public class GameService {
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(GameService.class);
     
+    public void saveRedis(BoardMain board){
+        redisTemplate.opsForValue().set(board.getName(), board);
+    }
+
+    public void createBoardMain(BoardMain board){
+
+    }
 }
